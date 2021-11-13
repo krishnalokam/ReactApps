9bb3d4639cc22fc1b38de0f48365ef29ebf80dde
sivalokam <skkumarlokam@gmail.com>
2021-11-13 20:30:28 +0530

    update index.js

--- react-basics/src/index.js
+++ react-basics/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 <<<<<<< HEAD
 import './App.css';
 
-const myElement = <h1 class="App"> Hello React </h1>
-=======
 import './index.css';
 import './App.css';
 
+const myElement = <h1 class="App"> Hello React </h1>
+
+
 const myElement = <div className="App"> <h1> Hello There </h1> </div>
->>>>>>> 4c7442abe0372df3ff156ec9410acac515ff836f
+
 
 ReactDOM.render(myElement,document.getElementById('root'));
