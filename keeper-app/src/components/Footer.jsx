4fbcf1b832f293c89e2dffd864fd25516557eac0
krishnalokam <siva.lokam4work@gmail.com>
2021-11-24 18:53:34 +0530

    commit

--- keeper-app/src/components/Footer.jsx
+++ keeper-app/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer>
-            <p className="footer">Copyright {new Date().getFullYear()}</p>
+            <p className="footer">Copyright &copy; {new Date().getFullYear()}</p>
         </footer>
     )
 }
